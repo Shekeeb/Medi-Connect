655dd1e0424b4ca9968470836f536c99af7786c4
Mohammed Shekeeb <shekeebmohammed10@gmail.com>
2024-05-15 11:18:07 +0530

    Update userController.js

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -201,8 +201,6 @@ export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
     .cookie("adminToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
-      secure:true,
-      sameSite:"None"
     })
     .json({
       success: true,
@@ -217,8 +215,6 @@ export const logoutPatient = catchAsyncErrors(async (req, res, next) => {
     .cookie("patientToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
-      secure:true,
-      sameSite:"None"
     })
     .json({
       success: true,
