20769be0ee02ccbabd8795eac9e76d5ce8f6584b
Mohammed Shekeeb <shekeebmohammed10@gmail.com>
2024-05-15 11:17:22 +0530

    Update jwtToken.js

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -9,8 +9,6 @@ export const generateToken = (user, message, statusCode, res) => {
           Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure:true,
-        sameSite:"None"
       })
       .json({
         success: true,
